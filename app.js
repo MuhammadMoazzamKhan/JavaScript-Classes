dd231750b367d595e26562c8d24e4219e2fb2eef
MuhammadMoazzamKhan <124440588+MuhammadMoazzamKhan@users.noreply.github.com>
2023-02-04 17:34:28 +0500

    JavaScript Chapter: 03

--- app.js
+++ app.js
@@ -1,24 +1,13 @@
-//Chapter # 2
-//Question (1) 
-var username = "Moazzam";
-//Question (2) 
-var myname = "Muhammad Moazzam Khan";
-//Question (3)
-var message = "Hello World";
-alert(message) ;
-//Question (4)
-var student = "15 years old";
-window.alert(student);
-//Question (5)
-alert("PIZZA \nPIZZ \nPIZ \nPI \nP ") 
-//Question (6)
-var email = "My email address is  example@example.com";
-alert(email);
-//Question (7)
-var book = "A smarter way to learn JavaScript.";
-alert(book);
-//Question (8)
-document.write("Yah! I can write HTML content through JavaScript")
-//Question (9)
-alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
-//End
+// Chapter # 2
+// Question (1) 
+// var age = "I'm 17 years old";
+// alert (age);
+// Question (2) 
+// var visited = "You visited my website 14 times";
+// alert(visited);
+// Question (3)
+// var birthYear = "My birth year is 2004"; 
+// document.write(birthYear);
+// Question (4)
+// var store = "John doe ordered 5 T-shirt(s) on XYZ clothing store";
+// document.write(store);
