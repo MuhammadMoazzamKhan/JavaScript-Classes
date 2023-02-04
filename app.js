c2abd3b009b8f5ba720b36e45b4ad7b6114900f9
MuhammadMoazzamKhan <124440588+MuhammadMoazzamKhan@users.noreply.github.com>
2023-02-04 18:26:05 +0500

    JavaScript Chapter: 05 to 09

--- app.js
+++ app.js
@@ -1,21 +1,153 @@
-// chaper # 4
-// Question (1)
-// var num1 = 28; 
-// var num2 = 12; 
-// var num3 = 5;
-// console.log(num1 + num2 + num3)
-// Question (2)
-// legal       |   illegal 
-// myNAme      |   Myname        
-// myFirstNAme |   Myfirstname        
-// name_2      |   name-2        
-// ligalName   |   Illigalname        
-// respnsiveWay|   Respnsiveway        
-// Question (3)
-// document.write("<h1>","Rules for naming JS variables","</h1>" );
-// document.write("<p>",num1," ",num2," ",num3,"</p>");
-// var  name1 = "$name";
-// var  name2 = "_name";
-// var  name3 = "name";
-// document.write("<p>",name1,",",name2," ","0r"," ",name3,"</p>");
+//Chapter # 5;
+// question(1);
+// var num = 3;
+// var num1 = 5;
+// document.write("Sum of ",num ," and ", num1 , " is ", num+num1 );
+
+// question(2)
+// var mod = num + num1;
+// console.log(mod);
+// var mod = num - num1;
+// console.log(mod);
+// var mod = num * num1;
+// console.log(mod);
+// var mod = num / num1;
+// console.log(mod);
+// var mod = num % num1;
+// console.log(mod);
+
+// question(3)
+// var name = 24;
+// document.write(name);
+// var num = 8;
+// document.write( "<p>",num - 3 ,"</p>");
+// num++ ;
+// document.write( "<h1>",num ,"</h1>");
+// num = num + 7 ;
+// document.write( "<h1>",num ,"</h1>");
+// num-- ;
+// document.write( "<h1>",num ,"</h1>");
+// num = num / 5;
+// document.write( "<h1>",num ,"</h1>");
+// document.write( "<h1>",num - 3  ,"</h1>");
+
+// question(4)
+// var ticket = 600 ;
+// console.log(ticket);
+// ticket = 600 * 5 ;
+// console.log(ticket)
+
+// question(5)
+// var num = 4 ;
+// document.write("<p>","Table of ", num, "</p>");
+// document.write("<p>","4 x 1 = ",num,"</p>");
+// document.write("<p>","4 x 2 = ",num*2 ,"</p>");
+// document.write("<p>","4 x 3 = ",num*3 ,"</p>");
+// document.write("<p>","4 x 4 = ",num*4 ,"</p>");
+// document.write("<p>","4 x 5 = ",num*5 ,"</p>");
+// document.write("<p>","4 x 6 = ",num*6 ,"</p>");
+// document.write("<p>","4 x 7 = ",num*7 ,"</p>");
+// document.write("<p>","4 x 8 = ",num*8 ,"</p>");
+// document.write("<p>","4 x 9 = ",num*9 ,"</p>");
+// document.write("<p>","4 x 10 = ",num*10 ,"</p>");
+
+// question(6)
+// var c = 12;
+// var f = 53;
+// var sum ;
+//  document.write(( f - 32 ) * 5 / 9,"<sup>0</sup> <br>" );
+//  document.write(( c * 9 / 5 ) + 32 ,"<sup>0</sup>");
+
+// question(7)
+// var item1 = 650 ; 
+// item1 = item1 * 3;
+// console.log("If item1 value is 650 , Quantity of item 1 is 3 = ",item1 )  ;
+// var item2 = 100 ;
+// document.write("<h1>Shopping Cart</h1>");
+// document.write("<p>Price of item 1 is 650</p>");
+// document.write("<p>Quantity of item 1 is 3</p>");
+// document.write("<p>Price of item 2 is 100</p>");
+// document.write("<p>Quantity of item 2 is 7</p>");
+// document.write("<p>Total cost of your order is 2750</p>");
+
+// question(8)
+// var totalMarks = 980;
+// var studentObtained = 804;
+// var percentage;
+// percentage = studentObtained * 100 / totalMarks;
+// document.write("<h1>Marks Sheet</h1>");
+// document.write("Total MArks:",totalMarks , "<br>");
+// document.write("MArks obtained:",studentObtained , "<br>");
+// document.write("Percentage : ",percentage, "% <br>");
+
+// question(9)
+// var usDolar = 104.84;
+// var saudiRiyal = 28;
+// var totalCurrency;
+// totalCurrency = usDolar* 10 + saudiRiyal* 25 ;
+// document.write("<h1>Currency In PKR</h1>");
+// document.write("Total currency In PKR : ", totalCurrency );
+
+// question(10)
+// var num1 = 2;
+// num1 = 2 + 5 * 10 / 2;
+// console.log(num1);
+
+// question(11)
+// var currentYear = 2023 ; 
+// var birthYear = 2002 ;
+// var  yourAge ;
+// yourAge = currentYear - birthYear ;
+// document.write("<h1>Age calculator</h1>" ); 
+// document.write("Current year : " ,currentYear,"<br>" ) ;
+// document.write("Birth year : ", birthYear ,"<br>" ) ;
+// document.write("Your age : " ,yourAge );
+
+// question(12)
+// var π = 3.142 ; 
+// var r = 20 ;
+// var area;
+// area = π * r * 2 ;
+// document.write("<h1> The Geometrizer</h1>");
+// document.write("Radius of a circle: ",r,"<br>" );
+// document.write("The circumference is: ",2*r*π ,"<br>");
+// document.write("The area is: 125.68");
+
+// question(13)
+// var snake = " cookie ";
+// var age = 18;
+// var maxAge = 30;
+// var perDaySmake = 10000 ;
+// var lifeTime;
+// var needCookie ;
+// needCookie =  (maxAge - age) * perDaySmake ;  
+// document.write("<h1>The Lifetime Supply Calculate</h1>");
+// document.write("Favourite Snake: ",snake,"<br>" );
+// document.write("Current age:",age,"<br>" );
+// document.write("Estimated Maximum Age: ",maxAge,"<br>" );
+// document.write("Amount of snacks per day: ",perDaySmake,"<br>" );
+// document.write("You'll need ",needCookie ,snake,"to last you until the ripe old age of ",maxAge,"<br>" );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
---
+++ app1.js
@@ -0,0 +1,57 @@
+//Chapter # 6-9;
+// question(1);
+// var num1 = 10 ;
+// document.write(num1, "<br>");
+// num1++ ;
+// document.write(num1, "<br>");
+// document.write(num1++, "<br>");
+// document.write(num1, "<br>");
+// document.write(++num1, "<br>");
+// document.write(++num1,--num1, "<br>");
+
+// question(2);
+// var a = 2, b = 1 ;
+// var result ;   
+// console.log(a,b);
+// result = --a - --b + ++b + b-- ;
+// console.log(a,b);
+// console.log(result);
+// document.write("a = ", a," <br>");
+// document.write("b = ",b ," <br>");
+// document.write("result = ", result," <br>");
+
+// question(3);
+// var fullName = prompt("Enter your name","As-salamu alaykum" );
+
+// question(4);
+// var num1 = +window.prompt("Enter a number");
+// var num2 = +window.prompt("Table of 5");
+// var sum ;
+// sum = (num1+num2) * 5;
+// alert(sum);
+
+
+// question(5);
+// var subject1 = "Math's" ;
+// var subject2 = "Science";
+// var subject3 = "Computer";
+// var totalMarks = 100;
+// var math = 90;
+// var sci = 88;
+// var comp = 73;
+// document.write("<h2>Subject Total Marks Obtained Marks Percentage</h2>")
+// document.write("<ul><li>" , subject1 ," ", totalMarks ," ",math," 90%","</li>" )
+// document.write("<li>" , subject2 ," ", totalMarks ," ",sci," 88%","</li>" )
+// document.write("<li>" , subject3 ," ", totalMarks ," ",comp," 73%","</li></ul>" )
+
+
+
+
+
+
+
+
+
+
+
+
