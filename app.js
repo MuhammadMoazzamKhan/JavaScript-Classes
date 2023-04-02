ba3b5fe4a18f7d931c8d16780ee122c3946045a9
MuhammadMoazzamKhan <124440588+MuhammadMoazzamKhan@users.noreply.github.com>
2023-04-02 15:54:57 +0500

    Zakat-assignment

--- app.js
+++ app.js
@@ -1,59 +1,22 @@
-// Question (1) //
-// var num = "5.509";
-// document.write("<h1>"+Number(num)+"</h1>");
-// document.write("<h1>"+Math.round(num)+"</h1>");
-// document.write("<h1>"+Math.floor(num)+"</h1>");
-// document.write("<h1>"+Math.ceil(num)+"</h1>");
-
-// Question (2) //
-// var num = -2.763;
-// document.write("<h1>"+Number(num)+"</h1>");
-// document.write("<h1>"+Math.round(num)+"</h1>");
-// document.write("<h1>"+Math.floor(num)+"</h1>");
-// document.write("<h1>"+Math.ceil(num)+"</h1>");
-
-// Question (3) //
-// function absoluteValue(number){
-//     if(number < 0){
-//         return -number;
-//     }else{
-//         return number;
-//     }
-// }
-// console.log(absoluteValue(-4));
-// console.log(absoluteValue(4));
-
-// Question (4) //
-// var num = Math.floor(Math.random()*7);
-// console.log(num)
-// console.log(num)
+function calculate() {
+    var gold = parseFloat(document.getElementById("gold").value * 207800);
+    var silver = parseFloat(document.getElementById("silver").value * 2521);
+    var wealth = parseFloat(document.getElementById("wealth").value);
+    if(gold + silver + wealth >= 132352.5){
+        var result = document.getElementById("result");
+        result.value =  (gold + silver + wealth) * 0.025 ;
+    }else{
+        alert("Zakat is not due on you");
+    }
+}
 
 
-// Question (5) //
-// var num = Math.floor(Math.random()*2);
 
-//  if(num < 1){
-//     document.write("random coin value : tails")
-//  }else{
-//     document.write("random coin value : Heads")
-//  }
 
-// Question (6) //
-// var num = Math.ceil(Math.random()*100);
-// document.write("random number between 1 and 100: "+num);
 
-// Question (7) //
-// var num = window.prompt("Enter your your weight .");
-// var result = parseInt(num);
-// var answer = result + Math.random();
-// var end = answer.toFixed(2);
-// document.write("The weight of user is "+end+" kilograms" );
 
-// Question (8) //
-// var num = 6;
-// var num1 = prompt("Enter a number between 1 and 10");
-// if (num == num1){
-//     alert("congratulation 🥰, You've win");
-// }else{
-//     alert("Oh Sorry  😁 , Please try again");
-// }
+
+
+
+
+
